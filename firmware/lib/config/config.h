b7286d7f8c934baeb67c858314a84adbcea45ffd
Sebastian Krebs <sebi.krebs@web.de>
2023-04-14 10:27:23 +0200

    change cores of tasks

--- firmware/lib/config/config.h
+++ firmware/lib/config/config.h
@@ -6,7 +6,7 @@ This Space is for defining important Variables like GPIO Pins.
 #define TASK0_PRIORITY					1
 #define TASK1_PRIORITY					1
 
-#define TASK0_CORE						0
-#define TASK1_CORE						1
+#define TASK0_CORE						1
+#define TASK1_CORE						0
 
 #endif
